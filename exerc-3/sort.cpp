235d2b630713bf2c5ccb06fe67f98ec4d01a9578
vitorstraggiotti <vitorstraggiotti@hotmail.com>
2021-10-28 14:28:41 -0300

    exercicio-3

---
+++ exerc-3/sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>			//cout
+#include <vector>			//vector
+#include <string>			//string
+#include <bits/stdc++.h>	//sort
+
+class Item
+{
+	public:
+	std::string data;
+	int key;
+	Item(const char * _data, int _key): data { _data }, key(_key) {}
+};
+
+std::vector<Item> items = {
+	{"john", 2},
+	{"bill", 3},
+	{"joe", 1},
+	{"sara", 5},
+	{"foo", 4}
+};
+
+void print_items(const std::vector<Item>items)
+{
+	for(auto & e: items)
+	{
+		std::cout << "-->" << e.key << ": " << e.data << "\n";
+	}
+}
+
+bool compareItem(Item& a, Item& b)
+{
+	return (a.key < b.key);
+}
+
+
+int main(void)
+{
+	std::cout << "--> Unordered:\n";
+	print_items(items);
+	std::sort(items.begin(), items.end(), compareItem);
+	std::cout << "--> Sorted:\n";
+	print_items(items);
+	
+	return 0;
+}
+
+
